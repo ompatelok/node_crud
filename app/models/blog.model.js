83ab06683255afb857c910be9f9488b63c31ddf1
Om Patel <72142020+ompatelok@users.noreply.github.com>
2025-06-11 12:46:29 +0530

    title and description added

--- app/models/blog.model.js
+++ app/models/blog.model.js
@@ -1,5 +1,19 @@
-const dbConf = require('../config/db.config')
-
 const mongoose = require("mongoose")
 
-mongoose.Promise = global.Promise
+var schema = new mongoose.Schema({
+    title:{
+        type:String,
+        requierd:true
+    },
+    description:{
+        type:String,
+        required:true
+    }
+
+},
+{
+    timestamps:true
+}
+);
+var blog = new mongoose.model('Blog', schema);
+module.export = blog;
